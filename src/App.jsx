853d870c36ebab44fb4810c549a163629e7f3fa4
majo024 <97540980+Mare024@users.noreply.github.com>
2024-03-06 22:36:41 -0600

    Creo que ya esta

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  const [userComments, setUserComments] = useState(new Map())
+  const [selectedUser, setSelectedUser] = useState(null)
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users/')
+      .then(response => {
+        setUsers(response.data)
+      })
+  }, [])
+
+  const handleClick = async (id) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/comments?userId=${id}`)
+    setUserComments(new Map(userComments.set(id, response.data)))
+    logComments(response.data)
+    setSelectedUser(id)
+  }
+
+  const logComments = (comments) => {
+    console.log(comments)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {users.map(user => (
+          <div key={user.id}>
+            <h2>{user.id}. {user.name}</h2>
+            <p>{user.phone}</p>
+            <button onClick={() => handleClick(user.id)}>
+              Show comments
+            </button>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {selectedUser && (
+        <div>
+          {userComments.get(selectedUser).map(comment => (
+            <div key={comment.id}>
+              <p>{comment.name}</p>
+              <p>{comment.email}</p>
+              <p>{comment.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   )
 }
